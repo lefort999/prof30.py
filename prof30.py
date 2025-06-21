820bd2291cc0213747a59b6e40b4ea9b5beb40d5
jacques <jlefort251@gmail.com>
2025-06-21 12:43:34 +0200

    prof30

--- prof30.py
+++ prof30.py
@@ -1,9 +1,12 @@
-@app.route("/")
-def recherche():
-    return render_template("index.html")
+import os
+from flask import Flask, request, render_template
 
 
+app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Hello, world!'
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # Render attribue automatiquement un port
